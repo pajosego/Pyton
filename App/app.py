b576b6e3bfed5b645f37ca1b5c548d4b7049033c
pajosego <144797657+pajosego@users.noreply.github.com>
2023-09-12 15:21:31 +0100

    Update app.py

--- App/app.py
+++ App/app.py
@@ -12,8 +12,20 @@ st.sidebar.header("Leagues")
 st.sidebar.header("Season")
 
 # WebScraping do Foottball Data, reunir informação dos csvs do site para fazer a base de dados: https://www.football-data.co.uk/mmz4281/2223/EO.csv //excel com a data da season 2023//
-# criar uma função para altearar as Leagues a e seasons de acordo com a nossa seleção que fizermos nos sidbars
+# criar uma função para altearar as Leagues a e seasons de acordo com a nossa seleção que fizermos nos sidbars, 
+# def = cria a função
+# load_data = nome da função
       
-def load_data(league, season): url =  "https://www.football-data.co.uk/mmz4281/" +season+ "/" +league+ ".csv"
+def load_data(league, season): 
+    url =  "https://www.football-data.co.uk/mmz4281/" +season+ "/" +league+ ".csv"
+
+#para ler a função, utiliza-se biblioteca pandas criando uma variavel data
+
+data = pd.read_csv(url)
+return data
+
+# data frame
+
+
 
 
