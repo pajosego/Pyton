6ec16fd039609678c94e3391af646eb85b5e9986
pajosego <144797657+pajosego@users.noreply.github.com>
2023-09-13 14:01:45 +0100

    Update app.py

--- App/app.py
+++ App/app.py
@@ -20,31 +20,32 @@ selected_season = st.sidebar.selectbox ('Season' , ['2022/2021' , '2021/2020', '
 #para ler a função, utiliza-se a biblioteca pandas criando uma variavel data     
 
 
-df = load_data(selected_league, selected_season)
- if selected_league == 'England':
-         league = 'E0'  
-  if selected_league == 'Germany':
-         league = 'D1'
-  if selected_league == 'Italy':
-         league = 'I1'
-  if selected_league == 'Spain':
-         league = 'SP1'
-  if selected_league == 'France':
-         league = 'F1'
+  # WebScraping Football Data
+    def load_data(league, season):
+      
+      if selected_league == 'England':
+        league = 'E0'
+      if selected_league == 'Germany':
+        league = 'D1'
+      if selected_league == 'Italy':
+        league = 'I1'
+      if selected_league == 'Spain':
+        league = 'SP1'
+      if selected_league == 'France':
+        league = 'F1'
+      
+      if selected_season == '2021/2022':
+        season = '2122'
+      if selected_season == '2020/2021':
+        season = '2021'
+      if selected_season == '2019/2020':
+        season = '1920'
+        
+      url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
+      data = pd.read_csv(url)
+      return data
 
-
-if selected_season == '2021/2022':
-      season = '2022'
-if selected_season == '2020/2021':
-      season = '2021'
-if selected_season == '2019/2020':
-      season = '2020'
-
-
-def load_data(league, season): 
-    url =  "https://www.football-data.co.uk/mmz4281/" +season+ "/" +league+ ".csv"
-    data = pd.read_csv (url)
-    return data
+    df = load_data(selected_league, selected_season)
 
 
 
